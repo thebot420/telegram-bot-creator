a30b2334ddee1ce0a2ac59381403f56faa444ba1
thebot420 <xxkjbellxx@gmail.com>
2025-08-03 13:30:20 +0100

    Definitive fix for all routes

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
-    from app import create_app, db
+from app import create_app, db
 
-    app = create_app()
+app = create_app()
 
-    # This adds our custom "init-db" command to the Flask CLI.
-    @app.cli.command("init-db")
-    def init_db_command():
-        """Creates the database tables."""
+# This adds our custom "init-db" command to the Flask CLI.
+@app.cli.command("init-db")
+def init_db_command():
+    """Creates the database tables."""
+    db.create_all()
+    print("Initialized the database.")
+
+# This block is only for running the server on your local computer.
+if __name__ == '__main__':
+    with app.app_context():
         db.create_all()
-        print("Initialized the database.")
-
-    # This block is only for running the server on your local computer.
-    if __name__ == '__main__':
-        with app.app_context():
-            db.create_all()
-        app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     
